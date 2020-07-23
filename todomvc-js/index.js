02c7132a529d70e608e58d261ef73287f818be0f
dat-lzt <2726219176@qq.com>
2020-07-23 18:36:19 +0800

    待增加全部、未完成、完成三个按钮相关功能，以及全选完成按钮bug修改

--- todomvc-js/index.js
+++ todomvc-js/index.js
@@ -2,11 +2,16 @@ import './assets/css/base.css'
 import './assets/css/index.css'
 
 
-var $ = function(id) {
-    return document.querySelector(id);
+var $ = function(sel) {
+    return document.querySelector(sel);
 };
-
+var $All = function(sel) {
+    return document.querySelectorAll(sel);
+};
 var uid = 0;
+var CL_COMPLETED = 'completed'; //完成
+var CL_EDITING = 'editing'; //编辑
+var CL_SELECTED = 'selected';
 
 function addTodo(todoText) {
     var todoList = $('.todo-list');
@@ -25,56 +30,119 @@ function addTodo(todoText) {
         '</div>'
     ].join('');
     console.log(todoText);
-
-    // var label = item.querySelector('.todo-label');
-    // label.addEventListener('dblclick', function() {
-    //     item.classList.add(CL_EDITING);
-
-    //     var edit = document.createElement('input');
-    //     var finished = false;
-    //     edit.setAttribute('type', 'text');
-    //     edit.setAttribute('class', 'edit');
-    //     edit.setAttribute('value', label.innerHTML);
-
-    //     function finish() {
-    //         if (finished) return;
-    //         finished = true;
-    //         item.removeChild(edit);
-    //         item.classList.remove(CL_EDITING);
-    //     }
-
-    //     edit.addEventListener('blur', function() {
-    //         finish();
-    //     });
-
-    //     edit.addEventListener('keyup', function(ev) {
-    //         if (ev.keyCode == 27) { // Esc
-    //             finish();
-    //         } else if (ev.keyCode == 13) {
-    //             label.innerHTML = this.value;
-    //             finish();
-    //         }
-    //     });
-
-    //     item.appendChild(edit);
-    //     edit.focus();
-    // }, false);
-
-    //标记为已完成
-    // item.querySelector('.toggle').addEventListener('change', function() {
-    //     updateTodo(id, this.checked);
-    // });
-
-    //删除
-    // item.querySelector('.destroy').addEventListener('click', function() {
-    //     removeTodo(id);
-    // });
-
     //在列表子元素之前插入新的子节点
     todoList.insertBefore(item, todoList.firstChild);
+    //将单个todo标为完成
+    item.querySelector('.toggle').addEventListener('change', function() {
+        updateTodo(id, this.checked);
+    });
+    //删除单个todo
+    item.querySelector('.destroy').addEventListener('click', function() {
+        removeTodo(id);
+    });
+
+    //双击lable进行编辑，增加按esc键取消编辑内容不变
+    //
+    var label = item.querySelector('.todo-label');
+    label.addEventListener('dblclick', function() {
+        item.classList.add(CL_EDITING); //编辑框样式
+
+        var edit = document.createElement('input');
+        var finshed = false;
+        edit.setAttribute('type', 'text');
+        edit.setAttribute('class', 'edit');
+        edit.setAttribute('value', label.innerHTML);
+
+        function finish() {
+            if (finshed) {
+                return;
+            }
+            finshed = true;
+            item.removeChild(edit);
+            item.classList.remove(CL_EDITING);
+
+        }
+        //当edit失去焦点是执行（‘blur’）
+        edit.addEventListener('blur', function() {
+            finish();
+        });
+        edit.addEventListener('keyup', function(ev) {
+            if (ev.keyCode == 27) { //ESC
+                finish();
+            } else if (ev.keyCode == 13) { //enter
+                if (!this.value.length) {
+                    removeTodo(id);
+                }
+                label.innerHTML = this.value;
+                finish();
+            }
+        });
+        item.appendChild(edit);
+        edit.focus();
+    }, false);
+    update();
 }
+//将单个todo标为完成
+function updateTodo(itemId, completed) {
+    //相当于document.querySelector(‘.toggle’);
+    var item = $('#' + itemId);
+    if (completed) {
+        item.classList.add(CL_COMPLETED);
+    } else {
+        item.classList.remove(CL_COMPLETED);
+    }
+    update();
+}
+//删除单个todo
+function removeTodo(itemId) {
+    var todoList = $('.todo-list');
+    var item = $('#' + itemId);
+    //DOM操作，删除列表中的第一个项目
+    todoList.removeChild(item);
+    update();
+}
+//清除已完成事件
+function ClearCompleted() {
+    var items = todoList.querySelectorAll('li');
+    for (var i = items.length - 1; i >= 0; --i) {
+        var item = items[i];
+        if (item.classList.contains(CL_COMPLETED)) {
+            todoList.removeChild(item);
+        }
+    }
+    update();
+}
+//全部标记为完成
+// function toggleAllTodoList() {
+//     var items = $All('.todo-list li');
+//     var toggleAll = $('.toggle-all');
+//     var checked = toggleAll.checked;
+//     for (var i = 0; i < items.length; ++i) {
+//         var item = items[i];
+//         var toggle = item.querySelector('.toggle');
+//         if (toggle.checked != checked) {
+//             toggle.checked = false;
+//             if (checked) item.classList.add(CL_COMPLETED);
+//             else item.classList.remove(CL_COMPLETED);
+//         }
+//     }
+// }
 
+function update() {
+    //计数：未完成个数
+    var items = $All('.todo-list li');
+    var leftNum = 0;
+    for (var i = 0; i < items.length; ++i) {
+        var item = items[i];
+        //contains用做判断返回值是ture还是false
+        if (!item.classList.contains(CL_COMPLETED)) {
+            leftNum++;
+        }
+    }
+    var count = $('.todo-count');
+    count.innerHTML = (leftNum || 'No') + (leftNum > 1 ? ' items' : ' item') + ' left';
 
+}
 
 window.onload = function init() {
     var newTodo = $('.new-todo'); // todo
@@ -86,9 +154,21 @@ window.onload = function init() {
             console.log('todoText is empty');
             return;
         }
-
         addTodo(todoText);
         newTodo.value = '';
     });
 
+    //清除已完成事件
+    var clearCompleted = $('.clear-completed');
+    clearCompleted.addEventListener('click', function() {
+        ClearCompleted();
+    });
+
+    //全部标记为完成
+    // var toggleAll = $('.toggle-all');
+    // toggleAll.addEventListener('change', function() {
+    //     toggleAllTodoList();
+    //     console.log(toggleAllTodoList);
+    // });
+    update();
 };
